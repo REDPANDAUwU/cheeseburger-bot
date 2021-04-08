786795655cca77c48fa6e772206afafa16157fa6
meow6969 <sdfsdfaasdfdcf@gmail.com>
2021-04-08 13:18:51 -0700

    Update onmsg.py

--- cogs/onmsg.py
+++ cogs/onmsg.py
@@ -96,7 +96,8 @@ class onmsg(commands.Cog):
 
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
-        await reaction.message.add_reaction(reaction)
+        if reaction.message.guild.id != 828797783863591012:
+            await reaction.message.add_reaction(reaction)
         if reaction.message.guild.id == 768371462489899028:
             channel = await user.create_dm()
             await channel.send('u added a  reaciton !!!!!!')
