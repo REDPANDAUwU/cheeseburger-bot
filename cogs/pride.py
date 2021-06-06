9c1abfadf0334311e1dfad42f38cfeb78395345f
meow6969 <75815262+meow6969@users.noreply.github.com>
2021-06-06 04:35:48 -0700

    Update pride.py

--- cogs/pride.py
+++ cogs/pride.py
@@ -54,9 +54,9 @@ class pride(commands.Cog):
         y = pic[0]['image']['geometry']['height']
         x += int(y/2)
         print(f'{x}:{y}')
-        os.system(f'magick convert -geometry {x}x{y} ./content/images/flags/pedo.png ./content/images/flags/temp/'
+        os.system(f'convert -geometry {x}x{y} ./content/images/flags/pedo.png ./content/images/flags/temp/'
                   f'{ctx.message.id}.png')
-        os.system(f'magick composite -compose multiply -gravity center ./content/images/flags/temp/'
+        os.system(f'composite -compose multiply -gravity center ./content/images/flags/temp/'
                   f'{ctx.message.id}.png {file_name} ./content/images/flags/temp/{ctx.message.id}output.png')
         await ctx.send(file=discord.File(f'./content/images/flags/temp/{ctx.message.id}output.png'))
 
